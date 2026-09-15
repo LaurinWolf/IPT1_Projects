c821598f3b8dff1416b82344e0010dd1ccc36d18
LaurinWolf <laurin_wolf@sluz.ch>
2026-09-15 16:02:01 +0200

    LS5-Mensa-Anwendung implementieren und PAP hinzufügen

---
+++ Praxisfall_Mensa_CSharp/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praxisfall_Mensa_CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string artikelname1;
+            string artikelname2;
+       
+            decimal einzelpreis1;
+            decimal einzelpreis2;
+
+            int menge1;
+            int menge2;
+
+            decimal mengenpreis1;
+            decimal mengenpreis2;
+
+            decimal gesamtpreis;
+
+            
+            
+            
+
+
+            Console.WriteLine("Artikelname eingeben");
+            artikelname1 = Console.ReadLine();
+            Console.WriteLine("Preis eingeben");
+            einzelpreis1 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Menge eingeben");
+            menge1 = Convert.ToInt32(Console.ReadLine());
+            mengenpreis1 = menge1 * einzelpreis1;
+            Console.WriteLine("Mengenpreis " + mengenpreis1);
+
+            //Console.Writeline("Mengenpreis: " + mengenpreis1;
+
+            Console.WriteLine("Artikelname eingeben");
+            artikelname2 = Console.ReadLine();
+            Console.WriteLine("Preis eingeben");
+            einzelpreis2 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Menge eingeben");
+            menge2 = Convert.ToInt32(Console.ReadLine());
+            mengenpreis2 = menge2 * einzelpreis2;
+            Console.WriteLine("Mengenpreis " + mengenpreis2);
+
+            //Console.Writeline("Mengenpreis: " + mengenpreis1;
+
+            Console.WriteLine(artikelname1 + " | " + einzelpreis1 + " | " + menge1 + " | " + mengenpreis1 + "\n" + artikelname2 + " | " + einzelpreis2 + " | " + menge2 + " | " + mengenpreis2);
+              
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
---
+++ Praxisfall_Mensa_CSharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("Praxisfall_Mensa_CSharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Praxisfall_Mensa_CSharp")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("e9852c16-563e-4af8-89a6-d537e6e5a1a7")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
